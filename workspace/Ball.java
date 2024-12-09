0474d8e51149aa1779b2a0129458814b1bfe5119
nxi181 <nailahgeorge1@gmail.com>
2024-12-09 14:14:10 +0000

    commit -m “submission”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,119 @@
+/* author: Nailah G.
+ * date: 12/4/24
+ * General description: How the ball moves and functions based on it's position in the game
+ */
+
 import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
 	//don't forget you need instance variables:
-	
+	private int Xpos;
+	private int Ypos;
+	private int size;
+	private int XVelocity = 2;
+	private int YVelocity = 2;
+
 	
 	//constructor(s):
+	public Ball (int initXPos, int initYPos, int initSize) {
+		Xpos = initXPos;
+		Ypos = initYPos;
+		size = initSize;
+	}
+
+	// getters and setters
+	public int getXpos() {
+		return Xpos;
+	}
+
+	public void setXpos(int Xpos) {
+		this.Xpos = Xpos;
+	}
+
+	public void setX (int x) {
+		x = Xpos;
+	}
+
+	public int getYpos() {
+		return Ypos;
+	}
+
+	public void setYPos(int Ypos) {
+		this.Ypos = Ypos;
+	}
+
+	public void setY(int y) {
+		y = Ypos;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize (int size) {
+		this.size = size;
+	}
+
+	public int getXVelocity () {
+		return XVelocity;
+	}
+
+	public void setXVelocity(int XVelocity) {
+		this.XVelocity = XVelocity;
+	}
+	
+	public int getYVelocity () {
+		return YVelocity;
+	}
+
+	public void setYVelocity(int YVelocity) {
+		this.YVelocity = YVelocity;
+	}
+	
 	
 	//methods:
 
+	// pre-condition: graphics class must be used/have no errors
+	// post-condition: Ball will be drawn with color
+	public void draw(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillOval( Xpos - size, Ypos - size, size * 2, size * 2);
+	}
+
+	// pre-condition: x and y velocity must be above 0
+	// post-condition: the x and y position changes based on the velocity of the ball and size/radius
+	public void move() {
+		Xpos += XVelocity;
+		Ypos += YVelocity;
+
+		if (Xpos <= size) {
+			Xpos = size;
+			reverseX();
+
+		}
+
+		if (Ypos <= size) {
+			Ypos = size;
+			reverseY();
+		}
+	}
+	// pre-condition: x velocity must be above 0
+	// post-condition: velocity is negative/object will go backwards
+	public void reverseX() {
+		XVelocity = -XVelocity;
+	}
+
+	// pre-condition: y velocity must be above 0
+	// post-condition: velocity is negative/object will go backwards
+	public void reverseY() {
+		YVelocity = -YVelocity;
+	}
+
+	
+
+	
+	
+
 }
+
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,97 @@
+/* author: Nailah G.
+ * date: 12/4/24
+ * General description: Controls how the player can move the paddle across the screen based on it's position on the map
+ * 
+ */
+
+
 import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
-
 	//your code here!
-	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int height;
+	private int width;
+	private int velocity = 0;
 	
 	//constructor(s):
-	
+	public Paddle(int initXPos, int initYPos, int initHeight, int initWidth ) {
+		x = initXPos;
+		y = initYPos;
+		height = initHeight;
+		width = initWidth;
+
+	}
+
+	//getters and setters
+	public int getX() {
+		return x ;
+	}
+
+	public void setX (int x){
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity (int velocity) {
+		this.velocity = velocity;
+	}
+
 	//methods:
+
+	// pre-condtition: graphics g class must be used/have no errors
+	//post-condition: Paddle will be drawn with color
+	public void draw(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+	}
+
+	// pre-condition:velocity must be 0 or above
+	//post-condition: the x position of the paddle moves based on the velocity
+	public void move() {
+		x += velocity;
+
+		if (x < 0) {
+			x = 0;
+		}
+		if (x + width > 500) {
+			x = 500 - width;
+		}
+	}
+	// pre-condition: velocity must be 0 or above
+	// post-condition: velocity increases based on xy parameter
+	
+	public void addVelocity ( int xy) {
+		velocity = xy;
+		x += velocity;
+
+		if (x < 0) {
+			x = 0;
+		}
+		if (x + width > 500) {
+			x = 500 - width;
+		}
+	}
+
+
 	
 	
 }
